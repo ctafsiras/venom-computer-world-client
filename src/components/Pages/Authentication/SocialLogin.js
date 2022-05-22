3de9b09f7b9c66000ffe020428aef48b2b4bcc96
Chowdhury Tafsir Ahmed Siddiki <ctafsiras@gmail.com>
2022-05-22 06:48:37 +0600

    business section

--- src/components/Pages/Authentication/SocialLogin.js
+++ src/components/Pages/Authentication/SocialLogin.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import auth from '../../../firebase.init';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
 
 const SocialLogin = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
     return (
         <div class="flex gap-4 item-center">
-            <button type="button" class="py-2 px-4 flex justify-center items-center  bg-green-600 hover:bg-green-700 focus:ring-green-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
+            <button
+                onClick={signInWithGoogle}
+                type="button" class="py-2 px-4 flex justify-center items-center  bg-green-600 hover:bg-green-700 focus:ring-green-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
                 Sign In With Google
             </button>
         </div>
